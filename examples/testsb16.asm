c70abc0e54645d6e8596ae60f1c98972bd76bf2e
Michael Urman <murman@gmail.com>
2003-04-18 21:53:04 +0000

    Write and use a printf macro; cuts file length down by 80 lines. Improves readability immensely ;)

--- examples/testsb16.asm
+++ examples/testsb16.asm
@@ -2,13 +2,35 @@
 ;  By Michael Urman, 2001
 ;
 ; Code history: 
+;  Update - 16bit works fine since VDMSound 2; reworked to use printf
+;	macro, which shortens the code considerably.
 ;  Original Code - only tests 8bit code, as sb16.asm doesn't yet do 16bit
-;  testsb16 - tests 16bit code
+;	testsb16 - tests 16bit code
 ;
-; $Id: testsb16.asm,v 1.3 2001/11/15 03:58:48 mu Exp $
+; $Id: testsb16.asm,v 1.4 2003/04/18 21:53:04 mu Exp $
 ;
 ; Note - the various _getchar, _printf calls are for example/testing
 ; convenience.  They are still not for use in final projects.
+
+
+; Mini "invoke _printf" that handles multiple arguments, all dwords
+; Note - you are probably not allowed to use _printf in your projects
+%macro printf 1-*
+	%rotate -1
+	%rep %0
+		%ifstr %1   ; handle strings
+			jmp	short %%endstr
+			%%str	db %1, 0
+			%%endstr
+			push	dword %%str
+		%else
+			push	dword %1
+		%endif
+		%rotate	-1
+	%endrep
+	call	_printf
+	add	esp, 4*%0
+%endmacro
 
 %include "lib291.inc"
 
@@ -49,15 +71,11 @@ _SoundISR
 _main
 	call	_LibInit
 
-	push	dword .dmalloc
-	call	_printf
-	add	esp, 4
+	printf	"[DMA_Alloc_Mem..."
 	invoke	_DMA_Allocate_Mem, dword SIZE, dword DMASel, dword DMAAddr
 	cmp	[DMASel], word 0
 	je	near .error
-	push	dword .done
-	call	_printf
-	add	esp, 4
+	printf	dword .done
 	invoke	_DMA_Lock_Mem
 
 	invoke	_LockArea, word [DMASel], dword 0, dword SIZE
@@ -68,71 +86,45 @@ _main
 	mov	esi, sine16
 	rep	movsd
 
-	push	dword .sbinit
-	call	_printf
-	add	esp, 4
+	printf	"[SB16_Init..."
 	invoke	_SB16_Init, dword _SoundISR
 	test	eax, eax
 	jnz	near .error
-	push	dword .done
-	call	_printf
-	add	esp, 4
+	printf	dword .done
 
-	push	dword .sbgetch
-	call	_printf
-	add	esp, 4
+	printf	"[SB16_GetChannel..."
 	invoke	_SB16_GetChannel
 	mov	[DMAChan], ah
+	push	eax
+	printf	dword .done
+	pop	eax
 	movzx	ecx, al
 	movzx	edx, ah
-	push	edx
-	push	ecx
-	push	dword .info
-		push	dword .done
-		call	_printf
-		add	esp, 4
-	call	_printf
-	add	esp, 12
+	printf	"[D%d H%d]%c%c", ecx, edx, 13, 10
 
 	; 8 bit 11kHz mono sound:
-	push	dword .sbsetf
-	call	_printf
-	add	esp, 4
+	printf	"[SB16_GetChannel..."
 	invoke	_SB16_SetFormat, dword 16, dword 11025, dword 0 
 	test	eax, eax
 	jnz	near .error
-	push	dword .done
-	call	_printf
-	add	esp, 4
+	printf	dword .done
 
-	push	dword .sbsetmix
-	call	_printf
-	add	esp, 4
+	printf	"[SB16_SetMixers..."
 	invoke	_SB16_SetMixers, word 07fh, word 07fh, word 07fh, word 07fh
 	test	eax, eax
 	jnz	near .error
-	push	dword .done
-	call	_printf
-	add	esp, 4
+	printf	dword .done
 
-	push	dword .dmastart
-	call	_printf
-	add	esp, 4
+	printf	"[DMA_Start..."
 	movzx	eax, byte [DMAChan]
 	invoke	_DMA_Start, eax, dword [DMAAddr], dword SIZE, dword 1, dword 1
 	; DMA_Start doesn't report error
-	push	dword .done
-	call	_printf
-	add	esp, 4
-	push	dword .sbstart
-	call	_printf
-	add	esp, 4
+	printf	dword .done
+	printf	"[SB16_Start..."
 	invoke	_SB16_Start, dword SIZE/2, dword 1, dword 1
 	test	eax, eax
 	jnz	near .error
-	push	dword .done
-	call	_printf
-	add	esp, 4
+	printf	dword .done
 
 	; here's where you'd normally wait to see if a full half buffer
 	; has been transferred (by probing a variable the ISR sets), and
@@ -148,94 +140,49 @@ _main
 
 	movzx	eax, byte [DMAChan]
 	invoke	_DMA_Todo, eax
-	push	eax
-	push	dword .dmatodo
-	call	_printf
-	add	esp, 8
+	printf	"**: DMA Todo: %d%c%c", eax, 13, 10
 	jmp	near .waitmore
 
 .stopit
-	push	dword .sbstartsc
-	call	_printf
-	add	esp, 4
+	printf	"[SB16_Start (Single Cycle)..."
 	invoke	_SB16_Start, dword SIZE/4, dword 0, dword 1
 	test	eax, eax
 	jnz	near .error
-	push	dword .done
-	call	_printf
-	add	esp, 4
+	printf	dword .done
 
-	push	dword .dmastop
-	call	_printf
-	add	esp, 4
+	printf	"[DMA_Stop..."
 	movzx	eax, byte [DMAChan]
 	invoke	_DMA_Stop, eax
 	; DMA_Stop doesn't report error
-	push	dword .done
-	call	_printf
-	add	esp, 4
+	printf	dword .done
 
-	push	dword .sbstop
-	call	_printf
-	add	esp, 4
+	printf	"[SB16_Stop..."
 	invoke	_SB16_Stop
 	; SB16_Stop doesn't report error
-	push	dword .done
-	call	_printf
-	add	esp, 4
+	printf	dword .done
 
-	push	dword .sbsetmix
-	call	_printf
-	add	esp, 4
+	printf	"[SB16_SetMixers..."
 	invoke	_SB16_SetMixers, word 0, word 0, word 0, word 0
 	test	eax, eax
 	jnz	near .error
-	push	dword .done
-	call	_printf
-	add	esp, 4
+	printf	dword .done
 
-	push	dword .sbexit
-	call	_printf
-	add	esp, 4
+	printf	"[SB16_Exit..."
 	invoke	_SB16_Exit
 	test	eax, eax
 	jnz	near .error
-	push	dword .done
-	call	_printf
-	add	esp, 4
+	printf	dword .done
 
 	call	_getchar
 
-	push	dword [ISR_Called]
-	push	dword .times
-	call	_printf
-	add	esp, 8
+	printf	"[Interrupted %d times]", dword [ISR_Called]
 	call	_LibExit
 	ret
 
 .error
-	push	dword .errmsg
-	call	_printf
-	add	esp, 4
+	printf	"Failed!] Exiting..."
 	mov	ax, 10
 	call	_LibExit
 	ret
 
-.errmsg		db	" Failed!]", 13,10,"Error", 13, 10, 0
-.dmalloc	db	"[DMA_Alloc_Mem...", 0
-.dmastart	db	"[DMA_Start...", 0
-.sbstart	db	"[SB16_Start...", 0
-.sbstartsc	db	"[SB16_Start (Single Cycle)...", 0
-.sbinit		db	"[SB16_Init...", 0
-.sbgetch	db	"[SB16_GetChannel...", 0
-.sbsetf		db	"[SB16_SetFormat...", 0
-.sbsetmix	db	"[SB16_SetMixers...", 0
-.dmastop	db	"[DMA_Stop...",0
-.sbstop		db	"[SB16_Stop...",0
-.sbexit		db	"[SB16_Exit...",0
 .done		db	" Done]", 13, 10, 0
-.info		db	"[D%d H%d]", 13, 10, 0
-.ver		db	"[V%d]", 13, 10, 0
-.times		db	"[Interrupted %d times]", 13, 10, 0
-.dmatodo	db	"**: DMA Todo: %d", 13, 10, 0
-
